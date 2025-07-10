82f3123f2165752c683547cba6ae75aeeaf15383
Shobhit150 <vshobhit150@gmail.com>
2025-07-10 13:25:36 +0530

    done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -24,7 +24,7 @@ const Hero = () => {
 
 
     return (
-        <div className='w-full h-[40vh] md:h-[75vh] relative'>
+        <div className='w-full h-[60vh] md:h-[75vh] relative'>
             <div>
                 <video
                     autoPlay
@@ -32,12 +32,13 @@ const Hero = () => {
                     muted
                     playsInline
                     preload="auto"
-                    className="w-full h-full object-cover absolute"
+                    className="h-full w-full object-cover absolute"
                 >
-                    <source src="/videos/output.mp4" type="video/mp4" />
+                    <source src="/videos/output4.mp4" type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
             </div>
+            
             <div className="absolute inset-0 bg-black/60 z-10" />
             <div className="relative z-20 flex items-center justify-center h-full text-white text-4xl font-bold flex-col text-center">
                 <div className="text-4xl md:text-[65px] font-bold mb-6 flex flex-col justify-center w-full ">
