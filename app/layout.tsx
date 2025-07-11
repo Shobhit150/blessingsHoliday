3ddb27752f9c492ddff0f5d42b5b69e19e79e8fa
Shobhit150 <vshobhit150@gmail.com>
2025-07-12 00:36:44 +0530

    fox: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,19 +57,23 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="google-site-verification" content="xqCFsqBWvYxFZ-yQRVtLwam1g7UQiUqqFl4U7tWg3Vk" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest"></link>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        
+
         <Navbar />
         {children}
         <BottomStickyBar />
-        <RightSideBar/>
+        <RightSideBar />
         <SpeedInsights />
         <Analytics />
       </body>
-      
+
     </html>
   );
 }
